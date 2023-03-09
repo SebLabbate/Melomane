90d3578de7e769ed10b3c8834f570ea4ae76d21a
audrone cirkelyte <audracirk@gmail.com>
2023-03-09 16:40:38 +0100

    not working edit and delete

--- config/routes.rb
+++ config/routes.rb
@@ -12,5 +12,6 @@ Rails.application.routes.draw do
   resources :gigs, only: :index do
     resources :user_gigs, only: :create
   end
-  resources :user_gigs, only: %i[index show]
+  resources :user_gigs
+  # only: %i[index show update]
 end
