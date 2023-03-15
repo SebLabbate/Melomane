69ace8ced5f8ca4c4b5c0f5dce059fcd6f1fc08c
Mazen El-Gonemy <mez.123@hotmail.co.uk>
2023-03-15 16:23:31 +0000

    play music

--- app/controllers/test2.rb
+++ app/controllers/test2.rb
@@ -1,20 +1,43 @@
-require "bundler"
-Bundler.require
-@accounts = Spotify::Accounts.new
-@accounts.client_id = "13a5a78c35794c128471c373008efb01"
-@accounts.client_secret = "1cfa0c2e5bc04728b9e9e56731e0db20"
-@accounts.redirect_uri = "http://localhost:3000/user_gigs/1"
-@accounts.authorize_url
-@session = @accounts.exchange_for_session(params[:code])
-if @session.expired?
-  @session.refresh!
-end
-@token = @session.refresh_token
-@session = Spotify::Session.from_refresh_token(@accounts, @token)
-@session.refresh!
-@sdk = Spotify::SDK.new(@session)
-@sdk.connect.devices
-@sdk.connect.devices[0].play!({
-  uri: "spotify:track:0tgVpDi06FyKpA1z0VMD4v",
-  position_ms: 0
-})
+require 'uri'
+require 'net/http'
+require 'openssl'
+
+
+#find track ID
+
+artist_first_name = "Post"
+artist_last_name = "Malone"
+song_name = "rockstar (feat. 21 Savage)"
+
+url = URI("https://soundcloud-scraper.p.rapidapi.com/v1/search/tracks?term=#{artist_first_name}%20#{artist_last_name}%20#{song_name}")
+
+http = Net::HTTP.new(url.host, url.port)
+http.use_ssl = true
+http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+
+request = Net::HTTP::Get.new(url)
+request["X-RapidAPI-Key"] = 'bb141578ecmsh6d28efe29ac7316p11e81cjsn2a1ed1dbaddf'
+request["X-RapidAPI-Host"] = 'soundcloud-scraper.p.rapidapi.com'
+
+response = http.request(request)
+string = response.read_body
+track_id_with_extras = string.split('"')[16]
+track_id_incomplete = track_id_with_extras.split(':')
+track_id = track_id_incomplete[1].split(',')
+track_id_two = track_id[0]
+#find track url
+
+url = URI("https://soundcloud-scraper.p.rapidapi.com/v1/track/metadata?track=#{track_id_two}&download=sq")
+
+http = Net::HTTP.new(url.host, url.port)
+http.use_ssl = true
+http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+
+request = Net::HTTP::Get.new(url)
+request["X-RapidAPI-Key"] = 'bb141578ecmsh6d28efe29ac7316p11e81cjsn2a1ed1dbaddf'
+request["X-RapidAPI-Host"] = 'soundcloud-scraper.p.rapidapi.com'
+
+response = http.request(request)
+body = response.read_body
+track_url = body.split('"')[11]
+puts track_url
