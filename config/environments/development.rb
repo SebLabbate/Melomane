caf70047d35b73ec3ebb643ef9dd26fe56fce6e9
audrone cirkelyte <audracirk@gmail.com>
2023-03-23 23:08:32 +0100

    video player working

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -60,6 +60,7 @@ Rails.application.configure do
   # Suppress logger output for asset requests.
   config.assets.quiet = true
 
+  config.assets.compile = true
   # Raises error for missing translations.
   # config.i18n.raise_on_missing_translations = true
 
@@ -68,4 +69,5 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
+
 end
