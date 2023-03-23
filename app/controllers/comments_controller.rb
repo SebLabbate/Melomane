93d68da8709c5cd929ed81680440a52d948cb631
audrone cirkelyte <audracirk@gmail.com>
2023-03-23 21:13:13 +0100

    users can now delete picture

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,5 +1,6 @@
 class CommentsController < ApplicationController
-  before_action :set_user_gig, only: %i[new show create destroy_attachment]
+  before_action :set_user_gig, only: %i[ new show create destroy_attachment]
+  before_action :set_comment, only: :destroy_attachment
 
   def index
     @comments = Comments.all.with_attached_photos
@@ -25,14 +26,20 @@ class CommentsController < ApplicationController
   end
 
   def destroy_attachment
-    attachment = ActiveStorage::Attachment.find(params[:photo_id])
+    attachment = ActiveStorage::Attachment.find(params[:attachment_id])
     attachment.purge
+    authorize @comment
+    redirect_to user_gig_path(@comment.user_gig)
   end
 
   private
 
   def set_user_gig
     @user_gig = UserGig.find(params[:user_gig_id])
+  end
+
+  def set_comment
+    @comment = Comment.find_by(params[:comment_id])
   end
 
   def comment_params
