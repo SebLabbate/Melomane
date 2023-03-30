5e5e78a763d3dda542750c05136e65faeaf3480c
Mazen El-Gonemy <mez.123@hotmail.co.uk>
2023-03-30 18:41:25 +0100

    find_gig

--- app/controllers/test.rb
+++ app/controllers/test.rb
@@ -3,26 +3,18 @@ require 'rspotify'
 require 'uri'
 require 'net/http'
 require 'openssl'
+require 'pexels'
 
-def find_events_array_by_name
-  url = URI("https://app.ticketmaster.com/discovery/v2/events.json?apikey=dQnJo7HE3HCwNKc3HbpQvCF3ps9exT7y&locale=*&city=london&classificationName=music")
-  http = Net::HTTP.new(url.host, url.port)
-  http.use_ssl = true
-  http.verify_mode = OpenSSL::SSL::VERIFY_NONE
-  request = Net::HTTP::Get.new(url)
-  response = http.request(request)
-  events = response.read_body
-  gigs = JSON.parse(events)
-  if gigs["_embedded"] != nil
-    events_hash = gigs["_embedded"]
-    events_hash_two = events_hash["events"]
-  else
-    events_hash_two = nil
+def pexel_photos
+  client = Pexels::Client.new('41EOfTlvkrnn8r297MvVFXPjmYq2jLs9OGSGZLfrQpDRmFVXMvMJdCHO')
+  photo = client.photos.search('concert').to_a
+  first = photo[rand(1..12)].src
+  array = []
+  first.each_value do |value|
+    array << value
   end
-  events_hash_two
-  event = events_hash_two[0]
-  artist_name = event["_embedded"]["attractions"][0]["name"]
-  return artist_name
+  photo_url = array[0]
+  return photo_url
 end
 
-puts artist = find_events_array_by_name
+puts pexel_photos
